e4dba40e8102549bcdcfe564c483abf2bdde5e11
rhythm1705 <rhythm.goel.17@outlook.com>
2019-10-22 10:05:54 -0400

    add imports

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill, intent_file_handler, intent_handler
+from adapt.intent import IntentBuilder
 
 
 class CricketScores(MycroftSkill):
